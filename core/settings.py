e0962ce5dba33499985c9c8b5c286a2f71a8c19d
Amantry7 <omurzakovomon@gmail.com>
2024-04-12 13:50:59 +0600

    startproject

--- core/settings.py
+++ core/settings.py
@@ -22,8 +22,7 @@ load_dotenv(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-udrvne*-8k&5@s+k6l2vp8fugv^_ybu1nxmf(t2g+43vb(lwhy'
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
